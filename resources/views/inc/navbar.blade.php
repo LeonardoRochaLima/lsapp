37757a99c440fe74f3df3e8717d09d7f11af796e
Your Name <leonardorocha.limaa@gmail.com>
2021-04-21 13:46:54 -0300

    Criando uma Navbar - Linkando as Páginas

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -7,13 +7,13 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="#">Project Name</a>
+            <a class="navbar-brand" href="/">{{config('app.name', 'LSAPP')}}</a>
         </div>
         <div id="navbar" class="collapse navbar-collapse">
             <ul class="nav navbar-nav">
-                <li class="active"><a href="#">Home</a></li>
-                <li><a href="#about">About</a></li>
-                <li><a href="#contact">Contact</a></li>
+                <li class="active"><a href="/">Home</a></li>
+                <li><a href="/about">About</a></li>
+                <li><a href="/services">Services</a></li>
             </ul>
         </div>
     </div>
