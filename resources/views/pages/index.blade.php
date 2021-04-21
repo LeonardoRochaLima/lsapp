5aa26a6926dabd90a1441fb559976cfd77354e3a
Your Name <leonardorocha.limaa@gmail.com>
2021-04-21 15:10:43 -0300

    Somente uma alteração na página principal

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
         <div class="jumbotron text-center">
-            <h1>Bem-vindo ao Laravel!</h1>
+            <h1>Bem-vindo ao Sistema de Laudos!</h1>
             <p>Essa é uma aplicação Laravel para teste.</p>
             <p><a class="btn-primary btn-lg" href="/login" role="button">Login</a> <a class="btn btn-success btn-lg" href="/register" role="button">Registrar</a></p>
 @endsection
