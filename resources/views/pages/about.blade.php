257df010d21149bc3fd7bbef5c35e6f5de7ff717
Your Name <leonardorocha.limaa@gmail.com>
2021-05-05 11:20:17 -0300

    Uma forma diferente de chamar o título da página

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1><?php echo $title; ?></h1>
-    <p>Essa é a página Sobre</p>
+    <h1>{{$title}}</h1>
+    <p>Essa é a págica Sobre.</p>
 @endsection
